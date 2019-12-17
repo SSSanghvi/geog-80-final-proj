285b0ceea888e5e7c40b76a7a664f3acac261db2
Sahil Sanghvi <sanghviss@berkeley.edu>
2019-12-16 23:28:37 -0800

    almost done?

--- carto_query_usda_fd.sql
+++ carto_query_usda_fd.sql
@@ -1,4 +1,4 @@
 # Before this I manually added a column named usda_fd to the database. 
 
 UPDATE la_only_grocery_stores 
-SET (latracts1 & lowincometracts) from la_only_grocery_stores
+SET usda_fd = (latracts1 & lowincometracts) from la_only_grocery_stores
